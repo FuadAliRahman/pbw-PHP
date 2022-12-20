5c22ce1edccc6b2fa6fc89247e15f228c94c1403
Fuad Ali Rahman <alirahmanfuad@gmail.com>
2022-12-20 07:24:12 +0700

    first commit

---
+++ DevFolio/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>DevFolio Bootstrap Portfolio Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: DevFolio - v4.9.1
+  * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php include 'header.php';?>
+><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <div id="hero" class="hero route bg-image" style="background-image: url(assets/img/hero-bg.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="hero-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="hero-title mb-4">I am Morgan Freeman</h1>
+          <p class="hero-subtitle"><span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+  </div><!-- End Hero Section -->
+
+    <?php include "main.php";?>
+ <!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include "footer.php";?>
+<!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ DevFolio/main.php
@@ -0,0 +1,31 @@
+<main id="main">
+
+<!-- ======= About Section ======= -->
+<?php include "main/about.php";?>
+<!-- End About Section -->
+
+<!-- ======= Services Section ======= -->
+<?php include "main/service.php";?>
+<!-- End Services Section -->
+
+<!-- ======= Counter Section ======= -->
+<?php include "main/counter.php";?>
+<!-- End Counter Section -->
+
+<!-- ======= Portfolio Section ======= -->
+<?php include "main/portofolio.php";?>
+<!-- End Portfolio Section -->
+
+<!-- ======= Testimonials Section ======= -->
+<?php include "main/testimoni.php";?>
+<!-- End Testimonials Section -->
+
+<!-- ======= Blog Section ======= -->
+<?php include "main/blog";?>
+<!-- End Blog Section -->
+
+<!-- ======= Contact Section ======= -->
+<?php include "main/contact";?>
+<!-- End Contact Section -->
+
+</main>
